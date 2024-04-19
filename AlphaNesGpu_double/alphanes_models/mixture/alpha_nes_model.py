solve bug gradients only first hidden

AlphaNesGpu_double/alphanes_models/mixture/alpha_nes_model.py
@@ -94,24 +94,35 @@ class alpha_nes_full(tf.Module):
         grad_2b=[tf.gradients(loss,physlay.alpha2b) for physlay in self.physics_layer]
         grad_3b=[tf.gradients(loss,physlay.alpha3b) for physlay in self.physics_layer]
         grad_mu=[tf.gradients(loss,lognorm.mu) for lognorm in self.lognorm_layer]
-        all_net_grad=[grad_w[k][0] for k in range(nt)]
-        all_net_param=[self.nets[k].trainable_variables[0] for k in range(nt)]
-        self.opt_net.apply_gradients(zip(all_net_grad,all_net_param))
+        
+        ########MODIFICA 19.04: SOLVE BUG ONLY FIRST HIDDEN ###########
+        #all_net_grad=[grad_w[k] for k in range(nt)]
+        #all_net_param=[self.nets[k].trainable_variables for k in range(nt)]
+        grads_and_vars_net=[]
+        for k in range(nt):
+            grad_var_pairs = [(grad, param) for grad, param in zip(grad_w[k], self.nets[k].trainable_variables)]
+            grads_and_vars_net.extend(grad_var_pairs)
+        #self.opt_net.apply_gradients(grads_and_vars_net)
+#        self.opt_net.apply_gradients(zip(all_net_grad,all_net_param))
+        grads_and_vars_afs = []
 
-        all_AFs_param=[self.physics_layer[k].alpha2b for k in range(nt)]
         for k in range(nt):
-            all_AFs_param.append(self.physics_layer[k].alpha3b)
-        for k in range(nt):
-            all_AFs_param.append(self.lognorm_layer[k].mu)
+            # Aggiungi i gradienti per alpha2b
+            grads_and_vars_afs.extend([(grad_2b[k][0], self.physics_layer[k].alpha2b)])
 
-        all_AFS_grad=[grad_2b[k][0] for k in range(nt)]
-        for k in range(nt):
-            all_AFS_grad.append(grad_3b[k][0])
-        for k in range(nt):
-            all_AFS_grad.append(grad_mu[k][0])
-        self.opt_phys.apply_gradients(zip(all_AFS_grad,
-                                              all_AFs_param))
+            # Aggiungi i gradienti per alpha3b
+            grads_and_vars_afs.extend([(grad_3b[k][0], self.physics_layer[k].alpha3b)])
+
+            # Aggiungi i gradienti per mu
+            grads_and_vars_afs.extend([(grad_mu[k][0], self.lognorm_layer[k].mu)])
+
+        grads_and_vars_all = grads_and_vars_afs + grads_and_vars_net
+
+        self.opt_net.apply_gradients(grads_and_vars_all)
+        
         self.global_step=self.global_step+1
+        ######## FINE MODIFICA 19.04: SOLVE BUG ONLY FIRST HIDDEN ###########
+
         return loss_force+loss_energy,loss_force,loss_energy
 
     @tf.function()
@@ -209,26 +220,34 @@ class alpha_nes_full(tf.Module):
         grad_3b=[tf.gradients(loss,physlay.alpha3b) for physlay in self.physics_layer]
         grad_mu=[tf.gradients(loss,lognorm.mu) for lognorm in self.lognorm_layer]
 
-        all_net_grad=[grad_w[k][0] for k in range(nt)]
-        all_net_param=[self.nets[k].trainable_variables[0] for k in range(nt)]
-        self.opt_net.apply_gradients(zip(all_net_grad,all_net_param))
+        ########MODIFICA 19.04: SOLVE BUG ONLY FIRST HIDDEN ###########
+        #all_net_grad=[grad_w[k] for k in range(nt)]
+        #all_net_param=[self.nets[k].trainable_variables for k in range(nt)]
+        grads_and_vars_net=[]
+        for k in range(nt):
+            grad_var_pairs = [(grad, param) for grad, param in zip(grad_w[k], self.nets[k].trainable_variables)]
+            grads_and_vars_net.extend(grad_var_pairs)
+        #self.opt_net.apply_gradients(grads_and_vars_net)
+#        self.opt_net.apply_gradients(zip(all_net_grad,all_net_param))
+        grads_and_vars_afs = []
 
+        for k in range(nt):
+            # Aggiungi i gradienti per alpha2b
+            grads_and_vars_afs.extend([(grad_2b[k][0], self.physics_layer[k].alpha2b)])
+            
+            # Aggiungi i gradienti per alpha3b
+            grads_and_vars_afs.extend([(grad_3b[k][0], self.physics_layer[k].alpha3b)])
+            
+            # Aggiungi i gradienti per mu
+            grads_and_vars_afs.extend([(grad_mu[k][0], self.lognorm_layer[k].mu)])
 
-        all_AFs_param=[self.physics_layer[k].alpha2b for k in range(nt)]
-        for k in range(nt):
-            all_AFs_param.append(self.physics_layer[k].alpha3b)
-        for k in range(nt):
-            all_AFs_param.append(self.lognorm_layer[k].mu)
+        grads_and_vars_all = grads_and_vars_afs + grads_and_vars_net
 
-        all_AFS_grad=[grad_2b[k][0] for k in range(nt)]
-        for k in range(nt):
-            all_AFS_grad.append(grad_3b[k][0])
-        for k in range(nt):
-            all_AFS_grad.append(grad_mu[k][0])
-        self.opt_phys.apply_gradients(zip(all_AFS_grad,
-                                              all_AFs_param))
+        self.opt_net.apply_gradients(grads_and_vars_all)
+        
         self.global_step=self.global_step+1
-        return loss,loss_energy,loss_bound,loss_force,grad_2b,grad_3b,grad_w
+        ######## FINE MODIFICA 19.04: SOLVE BUG ONLY FIRST HIDDEN ###########
+        return loss,loss_energy,loss_bound,loss_force,grad_2b,grad_3b,grad_mu,grad_w,[self.physics_layer[0].alpha2b,self.physics_layer[0].alpha3b,self.lognorm_layer[0].mu,self.nets[0].trainable_variables[0]]
 
     @tf.function()
     def full_test_e_f(self,x1,x2,x3bsupp,int2b,intder2b,int3b,intder3b,intder3bsupp,

AlphaNesGpu_double/optimizer_learning_rate_utility.py
@@ -7,6 +7,8 @@ import tensorflow.keras.optimizers.schedules as optsch
 import tensorflow.keras.experimental as tfexp
 import tensorflow.keras.optimizers as tfopt
 
+tf.keras.backend.set_floatx('float64')
+
 def build_learning_rate(param,ne,nb,buffer_stream_tr,name,num_call):
     if param[0]=='expdec':
        tf=ne*buffer_stream_tr
@@ -27,7 +29,7 @@ def build_learning_rate(param,ne,nb,buffer_stream_tr,name,num_call):
 
     elif param[0]=='cosann':
         try:
-            initial_learning_rate=float(param[1])
+            initial_learning_rate=np.float64(float(param[1]))
         except:
             initial_learning_rate=0.01
         try:
@@ -35,15 +37,15 @@ def build_learning_rate(param,ne,nb,buffer_stream_tr,name,num_call):
         except:
             first_decay_steps=int(2*nb*buffer_stream_tr)
         try:
-            t_mul=float(param[3])
+            t_mul=np.float64(float(param[3]))
         except:
             t_mul=2.0
         try:
-            m_mul=float(param[4])
+            m_mul=np.float64(float(param[4]))
         except:
             m_mul=1.0
         try:
-            alpha=float(param[5])
+            alpha=np.float64(float(param[5]))
         except:
             alpha=0.0
         if num_call==0:
@@ -53,8 +55,10 @@ def build_learning_rate(param,ne,nb,buffer_stream_tr,name,num_call):
             print("alpha_nes: ",name," t_mul set to ",t_mul,sep=' ',end='\n')
             print("alpha_nes: ",name," m_mul set to",m_mul,sep=' ',end='\n')
             print("alpha_nes: ",name," alpha set to",alpha,sep=' ',end='\n')
-        lr_built=tfexp.CosineDecayRestarts(initial_learning_rate,first_decay_steps,t_mul=t_mul,
+        lr_built=optsch.CosineDecayRestarts(initial_learning_rate,first_decay_steps,t_mul=t_mul,
                           m_mul=m_mul,alpha=alpha,name=None)
+        #tfexp.CosineDecayRestarts(initial_learning_rate,first_decay_steps,t_mul=t_mul,
+                          #m_mul=m_mul,alpha=alpha,name=None)
 
     else:
         sys.exit("alpha_nes: learning rate can be expdec or cosann.")

alphanes_models/mixture/alpha_nes_model.py
@@ -94,24 +94,34 @@ class alpha_nes_full(tf.Module):
         grad_2b=[tf.gradients(loss,physlay.alpha2b) for physlay in self.physics_layer]
         grad_3b=[tf.gradients(loss,physlay.alpha3b) for physlay in self.physics_layer]
         grad_mu=[tf.gradients(loss,lognorm.mu) for lognorm in self.lognorm_layer]
-        all_net_grad=[grad_w[k][0] for k in range(nt)]
-        all_net_param=[self.nets[k].trainable_variables[0] for k in range(nt)]
-        self.opt_net.apply_gradients(zip(all_net_grad,all_net_param))
 
-        all_AFs_param=[self.physics_layer[k].alpha2b for k in range(nt)]
+        ########MODIFICA 19.04: SOLVE BUG ONLY FIRST HIDDEN ###########
+        #all_net_grad=[grad_w[k] for k in range(nt)]
+        #all_net_param=[self.nets[k].trainable_variables for k in range(nt)]
+        grads_and_vars_net=[]
         for k in range(nt):
-            all_AFs_param.append(self.physics_layer[k].alpha3b)
-        for k in range(nt):
-            all_AFs_param.append(self.lognorm_layer[k].mu)
+            grad_var_pairs = [(grad, param) for grad, param in zip(grad_w[k], self.nets[k].trainable_variables)]
+            grads_and_vars_net.extend(grad_var_pairs)
+        #self.opt_net.apply_gradients(grads_and_vars_net)
+#        self.opt_net.apply_gradients(zip(all_net_grad,all_net_param))
+        grads_and_vars_afs = []
 
-        all_AFS_grad=[grad_2b[k][0] for k in range(nt)]
         for k in range(nt):
-            all_AFS_grad.append(grad_3b[k][0])
-        for k in range(nt):
-            all_AFS_grad.append(grad_mu[k][0])
-        self.opt_phys.apply_gradients(zip(all_AFS_grad,
-                                              all_AFs_param))
+            # Aggiungi i gradienti per alpha2b
+            grads_and_vars_afs.extend([(grad_2b[k][0], self.physics_layer[k].alpha2b)])
+
+            # Aggiungi i gradienti per alpha3b
+            grads_and_vars_afs.extend([(grad_3b[k][0], self.physics_layer[k].alpha3b)])
+
+            # Aggiungi i gradienti per mu
+            grads_and_vars_afs.extend([(grad_mu[k][0], self.lognorm_layer[k].mu)])
+
+        grads_and_vars_all = grads_and_vars_afs + grads_and_vars_net
+
+        self.opt_net.apply_gradients(grads_and_vars_all)
+        
         self.global_step=self.global_step+1
+        ######## FINE MODIFICA 19.04: SOLVE BUG ONLY FIRST HIDDEN ###########
         return loss_force+loss_energy,loss_force,loss_energy
 
     @tf.function()
@@ -209,25 +219,34 @@ class alpha_nes_full(tf.Module):
         grad_3b=[tf.gradients(loss,physlay.alpha3b) for physlay in self.physics_layer]
         grad_mu=[tf.gradients(loss,lognorm.mu) for lognorm in self.lognorm_layer]
 
-        all_net_grad=[grad_w[k][0] for k in range(nt)]
-        all_net_param=[self.nets[k].trainable_variables[0] for k in range(nt)]
-        self.opt_net.apply_gradients(zip(all_net_grad,all_net_param))
+        ########MODIFICA 19.04: SOLVE BUG ONLY FIRST HIDDEN ###########
+        #all_net_grad=[grad_w[k] for k in range(nt)]
+        #all_net_param=[self.nets[k].trainable_variables for k in range(nt)]
+        grads_and_vars_net=[]
+        for k in range(nt):
+            grad_var_pairs = [(grad, param) for grad, param in zip(grad_w[k], self.nets[k].trainable_variables)]
+            grads_and_vars_net.extend(grad_var_pairs)
+        #self.opt_net.apply_gradients(grads_and_vars_net)
+#        self.opt_net.apply_gradients(zip(all_net_grad,all_net_param))
+        grads_and_vars_afs = []
 
+        for k in range(nt):
+            # Aggiungi i gradienti per alpha2b
+            grads_and_vars_afs.extend([(grad_2b[k][0], self.physics_layer[k].alpha2b)])
 
-        all_AFs_param=[self.physics_layer[k].alpha2b for k in range(nt)]
-        for k in range(nt):
-            all_AFs_param.append(self.physics_layer[k].alpha3b)
-        for k in range(nt):
-            all_AFs_param.append(self.lognorm_layer[k].mu)
+            # Aggiungi i gradienti per alpha3b
+            grads_and_vars_afs.extend([(grad_3b[k][0], self.physics_layer[k].alpha3b)])
 
-        all_AFS_grad=[grad_2b[k][0] for k in range(nt)]
-        for k in range(nt):
-            all_AFS_grad.append(grad_3b[k][0])
-        for k in range(nt):
-            all_AFS_grad.append(grad_mu[k][0])
-        self.opt_phys.apply_gradients(zip(all_AFS_grad,
-                                              all_AFs_param))
+            # Aggiungi i gradienti per mu
+            grads_and_vars_afs.extend([(grad_mu[k][0], self.lognorm_layer[k].mu)])
+
+        grads_and_vars_all = grads_and_vars_afs + grads_and_vars_net
+
+        self.opt_net.apply_gradients(grads_and_vars_all)
+        
         self.global_step=self.global_step+1
+        ######## FINE MODIFICA 19.04: SOLVE BUG ONLY FIRST HIDDEN ###########
+
         return loss,loss_energy,loss_bound,loss_force
 
     @tf.function()