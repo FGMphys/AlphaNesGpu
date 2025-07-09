70eeef87b1599ac73aa920398052a3fac6a76f12
Francesco Guidarelli Mattioli <f.guidarellimattioli@gmail.com>
2025-07-09 17:41:35 +0200

    l

--- DEV/AlphaNesGpu_double_CG/init_params/init_AFs_param.py
+++ DEV/AlphaNesGpu_double_CG/init_params/init_AFs_param.py
@@ -56,19 +56,31 @@ def init_AFs_param(restart,full_param,number_of_interaction,seed_par):
         print("alpha_nes: alphas will be upper-bound to default",alpha_bound,sep=' ',end='\n')
     limit=alpha_bound
     limit3b=alpha_bound
-    if restart=='no':
-
+    if full_param['restart']=='no':
         [nalpha_r_arr,nalpha_a_arr,initial_type_emb,number_of_NN]=gen_map_type_AFs(full_param)
         
         ###Initialize radial AFS parameters
-        init_alpha2b=[(np.random.rand((nalpha_r_arr[k,1]*number_of_interaction))*2*limit-limit).reshape((number_of_interaction,nalpha_r_arr[k,1])).astype('float64') for k in range(number_of_NN)]
+        init_alpha2b=[]
+        for k in range(number_of_NN):
+            jump_xx=0
+            vec2b=np.zeros((number_of_interaction,nalpha_r_arr[k,1]))
+            alphas=(np.random.rand(nalpha_r_arr[k,1])*2*limit-limit).astype('float64')
+            for xx,param in enumerate(map_rad_afs[k]):
+                vec2b[xx,jump_xx:jump_xx+param]=alphas[jump_xx:jump_xx+param]
+                jump_xx=jump_xx+param
+            init_alpha2b.append(np.array(vec2b).reshape((number_of_interaction,nalpha_r_arr[k,1])))
         ###Initialize angular AFS parameters
         init_alpha3b=[]
         for k in range(number_of_NN):
-            vec=np.zeros((nalpha_a_arr[k,1]*nt_couple_interaction,3),dtype='float64')
-            vec[:,:2]=(np.random.rand((nalpha_a_arr[k,1]*nt_couple_interaction*2))*2*limit3b-limit3b).reshape((nalpha_a_arr[k,1]*nt_couple_interaction,2)).astype('float64')
-            vec[:,2]=(np.random.rand((nalpha_a_arr[k,1]*nt_couple_interaction))*-10).reshape(nalpha_a_arr[k,1]*nt_couple_interaction).astype('float64')
-            init_alpha3b.append(vec.reshape((nt_couple_interaction,nalpha_a_arr[k,1]*3)))
+            vec=np.zeros((nt_couple_interaction,nalpha_a_arr[k,1],3),dtype='float64')
+            delta_gamma=(np.random.rand((nalpha_a_arr[k,1]*2))*2*limit3b-limit3b).astype('float64')
+            beta=(np.random.rand(nalpha_a_arr[k,1])*-10).astype('float64')
+            jump_xx=0
+            for xx,param in enumerate(map_ang_afs[k]):
+                vec[xx,jump_xx:jump_xx+param,:2]=delta_gamma[jump_xx*2:(jump_xx+param)*2].reshape((-1,2))
+                vec[xx,jump_xx:jump_xx+param,2]=beta[jump_xx:jump_xx+param]
+                jump_xx=jump_xx+param
+            init_alpha3b.append(np.array(vec).reshape((nt_couple_interaction,nalpha_a_arr[k,1]*3)))
         #Initialised Z for each AFS
         init_mu=[(np.random.rand(nalpha_r_arr[k,1]+nalpha_a_arr[k,1])*2*limit-limit).astype('float64')
                 for k in range(number_of_NN)]
@@ -76,7 +88,11 @@ def init_AFs_param(restart,full_param,number_of_interaction,seed_par):
     else :
         map_rad_afs=full_param['map_rad_afs']
         number_of_NN=len(map_rad_afs)
-        afs_param=full_param['params_folder']
+        if full_param['restart']==['only_afs','full_param']:
+           afs_param=full_param['params_folder']
+        else:
+           afs_param=restart
+        print("alpha_nes: AF parameters load from folder ",afs_param) 
         init_mu=[np.loadtxt(afs_param+'/type'+str(k)+'_alpha_mu.dat',dtype='float64') for k in range(number_of_NN)]
         init_alpha2b=[np.loadtxt(afs_param+'/type'+str(k)+'_alpha_2body.dat',dtype='float64').reshape((number_of_interaction,-1)) for k in range(number_of_NN)]
         init_alpha3b=[np.loadtxt(afs_param+'/type'+str(k)+'_alpha_3body.dat',dtype='float64').reshape((nt_couple_interaction,-1)) for k in range(number_of_NN)]
@@ -95,4 +111,4 @@ def init_AFs_param(restart,full_param,number_of_interaction,seed_par):
         print("alpha_nes:      ",nalpha_a_arr[k,0],"        ",nalpha_a_arr[k,1])
 
     return init_alpha2b,init_alpha3b,init_mu,initial_type_emb,np.random.get_state()
-
+breakpoint()
